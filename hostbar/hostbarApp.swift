c32458745e400dc9c9ccdf42fa111bb7d0572a17
joododok <shield41791@gmail.com>
2026-04-17 01:30:33 +0900

    Initial Commit

---
+++ hostbar/hostbarApp.swift
@@ -0,0 +1,17 @@
+//
+//  hostbarApp.swift
+//  hostbar
+//
+//  Created by Yohan Joo on 2026/04/17.
+//
+
+import SwiftUI
+
+@main
+struct hostbarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
